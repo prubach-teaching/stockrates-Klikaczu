b8470e90e99770d7af23b49f5b6c91ee33e06a73
Klikaczu <54986167+Klikaczu@users.noreply.github.com>
2020-06-16 14:31:57 +0200

    Update StockData.java

--- src/main/java/sgh/StockData.java
+++ src/main/java/sgh/StockData.java
@@ -11,30 +11,32 @@ public class StockData {
 
     public static void getAndProcessChange(String stock) throws IOException {
         String filePath = "data_in/" + stock + ".csv";
-        File stockDataFile=new File(filePath);
-         // Checking if the file has already been created
-        if (!stockDataFile.exists()){
+        File stockDataFile = new File(filePath);
+        // Checking if the file has already been created
+        if (!stockDataFile.exists()) {
             download("https://query1.finance.yahoo.com/v7/finance/download/" + stock +
-                    "?period1=1554504399&period2=1586126799&interval=1d&events=history",filePath);
+                    "?period1=1554504399&period2=1586126799&interval=1d&events=history", filePath);
         }
-        
+
         Scanner sc = new Scanner(stockDataFile);
         String line = sc.nextLine();
-        
+
         // New data file
-        
-        FileWriter newDataFile=new FileWriter("data_out/" + stock + ".csv");
+
+        FileWriter newDataFile = new FileWriter("data_out/" + stock + ".csv");
         newDataFile.write(line + ",Change\n");
-        while (sc.hasNextLine()){
-            line=sc.nextLine();
-            String[] elements=line.split(",");
-            double openPrice=Double.valueOf(elements[1]);
-            double closePrice=Double.valueOf(elements[4]);
-            double changeInPrice=(closePrice - openPrice) / openPrice;
+
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+            String[] elements = line.split(",");
+            double openPrice = Double.valueOf(elements[1]);
+            double closePrice = Double.valueOf(elements[4]);
+            double changeInPrice = (closePrice - openPrice) / openPrice;
+
             newDataFile.write(line + "," + changeInPrice * 100 + "\n");
         }
+        sc.close();
         newDataFile.close();
-        
     }
 
     public static void download(String url, String fileName) throws IOException {
